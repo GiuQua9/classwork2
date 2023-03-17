b592e53adda93f9b11584b77eef2aa91c5756b9a
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-03-17 18:11:51 +0100

    funziona

--- src/control_loop.h
+++ src/control_loop.h
@@ -5,11 +5,21 @@ using namespace std;
 
 class CONTROLLER {
     public:
-        CONTROLLER();
+        CONTROLLER(float, float, float);
         
-        void loop();                //Main loop function        
+        void loop();                //Main loop function
+        
         void system_start();       //start the system
         void set_xdes(double x);   //member to set the desired value
 
     private:
+        double _xdes; //desired value to reach
+        double _xmes; //current value of my system
+        bool   _system_init; //boolean flag
+        
+        double _kp; // PID gains 
+        double _kd; // PID gains
+        double _ki; // PID gains
+
+        bool _first_des_value;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
+/*#include "control_loop.h"
+
+using namespace std;
+
+//the goal is to implement a simple PID controller, 
+//running on input value to reach the desired one
+
+// Sense: read a value from keyboard
+// Plan:  generate the correct input
+// Act:   set the input
+
+int main(int argc, char** argv) {
+
+
+    return 0;
+}
+*/
+
 #include "control_loop.h"
 
 using namespace std;
@@ -11,6 +29,18 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
+    //New controller with the given gains for the PID
+    CONTROLLER c(0.1,0.05,0.01);
+
+    double x;
+    while( true ) {
+        cout << "Insert desired value for the controller: ";
+        cin >> x;
+        c.set_xdes(x);
+    }
+    
+    //What happen without the while loop?
+    //Should I wait after the input?
 
     return 0;
 }
